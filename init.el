bdcbc8d6e80f6ba95a8a5b17c3da2537f4a969fe
Terence An <absenteeTerence@gmail.com>
2019-08-29 20:15:38 -0400

    update init.el

--- init.el
+++ init.el
@@ -31,7 +31,10 @@ There are two things you can do about this warning:
     ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
  '(package-selected-packages
    (quote
-    (magit color-theme-sanityinc-tomorrow counsel swiper ivy))))
+    (elpy company-quickhelp company-lsp exec-path-from-shell lsp-rust cargo flycheck flycheck-rust lsp-mode lsp-ui rust-mode toml-mode company company-jedi magit color-theme-sanityinc-tomorrow counsel swiper ivy))))
+
+(require 'use-package)
+
 
 ;; Use M-x describe-face to change the face under your cursor
 (custom-set-faces
@@ -39,11 +42,18 @@ There are two things you can do about this warning:
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "color-233" :foreground "#c5c8c6" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "default"))))
- '(custom-comment-tag ((t (:foreground "color-173"))))
- '(font-lock-comment-delimiter-face ((t (:foreground "color-39" :slant italic))))
- '(font-lock-comment-face ((t (:foreground "#6fd780" :slant italic))))
- '(font-lock-keyword-face ((t (:foreground "color-39")))))
+ '(default ((t (:inherit nil :stipple nil :background "#121212" :foreground "#fcf9ea" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "default"))))
+ '(font-lock-builtin-face ((t (:foreground "#b041ad"))))
+ '(font-lock-comment-delimiter-face ((t (:foreground "#badfdb" :slant italic))))
+ '(font-lock-comment-face ((t (:foreground "#badfdb" :slant italic))))
+ '(font-lock-function-name-face ((t (:foreground "#ff8a5c"))))
+ '(font-lock-keyword-face ((t (:foreground "#49beb7"))))
+ '(font-lock-string-face ((t (:foreground "#d1eecc")))))
+
+;; Generic Configuration
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+;; globals
+(global-linum-mode t) ;; enable line numbers globally
 
 ;; Ivy configuration
 (ivy-mode 1)
@@ -57,6 +67,7 @@ There are two things you can do about this warning:
 (global-set-key (kbd "<f6>") 'ivy-resume)
 (global-set-key (kbd "M-x") 'counsel-M-x)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "C-f") 'counsel-file-jump)
 (global-set-key (kbd "<f1> f") 'counsel-describe-function)
 (global-set-key (kbd "<f1> v") 'counsel-describe-variable) ;; C-h v
 (global-set-key (kbd "<f1> l") 'counsel-find-library)
@@ -72,3 +83,97 @@ There are two things you can do about this warning:
 (setq ivy-re-builders-alist
       '((read-file-name-internal . ivy--regex-fuzzy)
         (t . ivy--regex-plus)))
+
+
+;; LANGUAGE MODE SETUPS
+;; generic
+(use-package flycheck
+  :hook (prog-mode . flycheck-mode))
+
+(use-package company
+  :hook (prog-mode . company-mode)
+  :config (setq company-tooltip-align-annotations t)
+  (setq company-minimum-prefix-length 1)
+  (setq company-idle-delay 0.1)
+  (company-quickhelp-mode 1)
+  (setq company-quickhelp-delay 0.1)
+  (setq company-show-numbers t))
+
+;; Bind numbers when company active to select
+;; rebind space for completion
+;; but prefer number in autocomplete over selection
+(defun ora-company-number ()
+  "Forward to `company-complete-number'.
+
+Unless the number is potentially part of the candidate.
+In that case, insert the number."
+  (interactive)
+  (let* ((k (this-command-keys))
+         (re (concat "^" company-prefix k)))
+    (if (or (cl-find-if (lambda (s) (string-match re s))
+                        company-candidates)
+            (> (string-to-number k)
+               (length company-candidates)))
+        (self-insert-command 1)
+      (company-complete-number
+       (if (equal k "0")
+           10
+         (string-to-number k))))))
+
+(with-eval-after-load 'company
+  (let ((map company-active-map))
+    ;; Remap Meta next and prev to ctrl next and prev
+    (define-key map (kbd "M-n") nil)
+    (define-key map (kbd "M-p") nil)
+    (define-key map (kbd "C-n") #'company-select-next)
+    (define-key map (kbd "C-p") #'company-select-previous)
+    ;; remap number selection to the ora kind
+    (mapc (lambda (x) (define-key map (format "%d" x) 'ora-company-number))
+	  (number-sequence 0 9))
+    (define-key map " " (lambda ()
+			  (interactive)
+			  (company-abort)
+			  (self-insert-command 1)))
+    (define-key map (kbd "<return>") nil)
+    (define-key company-active-map (kbd "C-c h") #'company-quickhelp-manual-begin)
+  ))
+
+(provide 'ora-company)
+
+;; Use LSP
+(use-package lsp-mode
+  :commands lsp
+  :config (require 'lsp-clients))
+;; For now we set lsp-enable-snippit to false because I don't find yasnippet necessary
+;; We may re-explore this in the future.
+(setq lsp-enable-snippet nil)
+
+(use-package lsp-ui)
+
+;; Python
+(setq python-shell-interpreter "/usr/local/bin/python")
+(defun my/python-mode-hook ()
+  ;; company-jedi isn't showing documentation
+  ;; (add-to-list 'company-backends 'company-jedi)
+  (use-package elpy
+    :ensure t
+    :init
+    (elpy-enable))
+  (local-set-key (kbd "C-c f") 'elpy-goto-definition)
+  (local-set-key (kbd "C-c F") 'elpy-goto-definition-other-window)
+  )
+(add-hook 'python-mode-hook 'my/python-mode-hook)
+
+;; Rust
+
+;; (use-package toml-mode)
+
+;; (use-package rust-mode
+;;   :hook (rust-mode . lsp))
+
+;; ;; Add keybindings for interacting with Cargo
+;; (use-package cargo
+;;   :hook (rust-mode . cargo-minor-mode))
+
+;; (use-package flycheck-rust
+;;   :config (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
